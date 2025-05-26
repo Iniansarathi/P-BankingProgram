44afd382404d6ea0e3c22f754632ee4c1ff7bd9b
Iniansarathi <iniansarathi2003@gmail.com>
2025-05-26 23:08:46 +0530

    Created input validation for userchoise

--- BankingProgram.py
+++ BankingProgram.py
@@ -1,7 +1,13 @@
 def userchoice ():
     print()
     uchoice = input("Enter your Choice [1-4]: ")
-
+    if uchoice.isdigit() :
+        uchoice = int(uchoice)
+        if uchoice > 0 and uchoice <5:
+            print("hi")
+        else:
+            print("Invalid input")
+            userchoice()
 def showbalance():
     pass
 def withdraw():
