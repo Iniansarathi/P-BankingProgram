4a3546f268a9b4584b8daf9745813a6063ffb579
Iniansarathi <iniansarathi2003@gmail.com>
2025-05-26 22:59:53 +0530

    created the basic structure of the program.

--- BankingProgram.py
+++ BankingProgram.py
@@ -1,1 +1,34 @@
+def userchoice ():
+    print()
+    uchoice = input("Enter your Choice [1-4]: ")
 
+def showbalance():
+    pass
+def withdraw():
+    pass
+def deposite():
+    pass
+def quit():
+    pass
+def main():
+
+    print("1. Show Balance")
+    print("2. Withdraw")
+    print("3. Deposite")
+    print("4. Quit")
+
+    userchoice()
+
+    global balance
+    balance = 83068.65
+
+
+
+
+if __name__ == '__main__':
+    print()
+    print("*********************************")
+    print("   Welcome To Banking Program")
+    print("*********************************")
+    print()
+    main()
